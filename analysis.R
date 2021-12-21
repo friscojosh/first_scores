d3073a51046f286894bedb12870d82b4dbad53b7
Josh Hermsmeyer <friscojosh.0@gmail.com>
2021-12-21 13:40:01 -0800

    Initial commit

---
+++ analysis.R
@@ -0,0 +1,40 @@
+library(tidyverse)
+library(psych)
+
+# first score: https://nfl-football.espntrumedia.com/football/teams-summary-stats?f=%7B%22fswr%22%3A%5B%7B%22fromSeason%22%3A2020%2C%22fromWeek%22%3A%221%22%2C%22toSeason%22%3A2020%2C%22toWeek%22%3A%2218%22%7D%5D%2C%22fgt%22%3A%5B%22regular%22%5D%7D&pc=%7B%22fsv%22%3A%22stats%22%2C%22fgb%22%3A%22by-season%22%2C%22ftt%22%3A%22standard%22%7D&t=%7B%22customReport%22%3A%7B%22selectedReportName%22%3A%22Scoring%2520First%22%2C%22selectedReportId%22%3A10009%7D%2C%22orderBy%22%3A%7B%22orderCols%22%3A%22Win%2525_Score1st%22%2C%22sortOrder%22%3A%22DESC%22%7D%7D
+# epa: https://nfl-football.espntrumedia.com/football/teams-summary-stats?f=%7B%22fswr%22%3A%5B%7B%22fromSeason%22%3A2006%2C%22fromWeek%22%3A%221%22%2C%22toSeason%22%3A2020%2C%22toWeek%22%3A%2218%22%7D%5D%2C%22fgt%22%3A%5B%22regular%22%5D%7D&pc=%7B%22fsv%22%3A%22stats%22%2C%22fgb%22%3A%22by-season%22%2C%22ftt%22%3A%22standard%22%7D&t=%7B%22customReport%22%3A%7B%22selectedReportName%22%3A%22rush%2520pass%2520epa%22%2C%22selectedReportId%22%3A592%7D%2C%22orderBy%22%3A%7B%22orderCols%22%3A%22%255BOfSnap%255D%22%2C%22sortOrder%22%3A%22DESC%22%7D%7D
+# 2021 first score: https://nfl-football.espntrumedia.com/football/teams-summary-stats?f=%7B%22fswr%22%3A%5B%7B%22fromSeason%22%3A%22def%22%2C%22fromWeek%22%3A%221%22%2C%22toSeason%22%3A%22def%22%2C%22toWeek%22%3A%2218%22%7D%5D%2C%22fgt%22%3A%5B%22regular%22%5D%7D&pc=%7B%22fsv%22%3A%22stats%22%2C%22fgb%22%3A%22by-season%22%2C%22ftt%22%3A%22standard%22%7D&t=%7B%22customReport%22%3A%7B%22selectedReportName%22%3A%22Scoring%2520First%22%2C%22selectedReportId%22%3A10009%7D%2C%22orderBy%22%3A%7B%22orderCols%22%3A%22Win%2525_Score1st%22%2C%22sortOrder%22%3A%22DESC%22%7D%7D
+
+first_scores <- read_csv("data/first_score_2000-20.csv") |>
+  mutate(first_score_pct = G_Score1st / G,
+         win_pct = `Win%`) |>
+  select(-Rank, -league, -teamImageId, -teamFullName, -teamAbbrevName, -espnTeamId, -G, -teamId_1, -null, -seasonId, -scatterId, -scatterExtra)
+
+first_scores |>
+  ggplot(aes(x = G_Score1st, y = W)) +
+  geom_point() +
+  geom_smooth()
+
+first_scores |>
+  ggplot(aes(x = first_score_pct, y = win_pct)) +
+  geom_point() +
+  geom_smooth(method = "lm") +
+  labs(x = "First score %", y = "Win %") +
+  theme_minimal()
+
+model <- lm(data = first_scores, win_pct ~ first_score_pct)
+summary(model)
+
+epa_data <- read_csv("data/epa_2006-20.csv")|>
+  mutate(comp_pct = parse_number(`Comp%`),
+         first_down_pct = parse_number(`1D%`),
+         success_rate = parse_number(`Scs%`)) |>
+  select(-Rank, -league, -teamImageId, -teamFullName, -teamAbbrevName, -espnTeamId, -G, -teamId_1, -null, -seasonId, -scatterId, -scatterExtra,
+         -`1D%`, -`Comp%`, -`Scs%`)
+# join team offense data to the score first dataframe
+joined <- first_scores |>
+  inner_join(epa_data, by = c("teamId", "team", "season"))
+
+corPlot(joined[, 19:32], scale = FALSE)
+# The highest correlation is with Pass EPA/play.
+
