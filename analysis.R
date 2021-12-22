604286b4c4b49dfa47a3a1f46a7127c1939f7a0b
Josh Hermsmeyer <friscojosh.0@gmail.com>
2021-12-22 02:04:33 -0800

    Add code for week to week analysis

--- analysis.R
+++ analysis.R
@@ -36,5 +36,20 @@ joined <- first_scores |>
   inner_join(epa_data, by = c("teamId", "team", "season"))
 
 corPlot(joined[, 19:32], scale = FALSE)
-# The highest correlation is with Pass EPA/play.
+# Read the plot as just the first column, and downwards.
+# The highest correlation besides win% is with Pass EPA/play.
 
+
+# https://nfl-football.espntrumedia.com/football/teams-summary-stats?f=%7B%22fswr%22%3A%5B%7B%22fromSeason%22%3A%22def%22%2C%22fromWeek%22%3A1%2C%22toSeason%22%3A%22def%22%2C%22toWeek%22%3A16%7D%5D%2C%22fgt%22%3A%5B%22regular%22%5D%7D&pc=%7B%22fsv%22%3A%22stats%22%2C%22fgb%22%3A%22by-game%22%2C%22ftt%22%3A%22standard%22%7D&t=%7B%22customReport%22%3A%7B%22selectedReportName%22%3A%22Scoring%2520First%22%2C%22selectedReportId%22%3A10009%7D%2C%22orderBy%22%3A%7B%22orderCols%22%3A%22Win%2525_Score1st%22%2C%22sortOrder%22%3A%22DESC%22%7D%7D
+
+weeks <- read_csv("data/first_score_by_week.csv") |>
+  filter(G_Score1st == 1) |> # look just at teams that scored first in a game
+  group_by(week) |>
+  summarize(win_percentage = sum(W) / n())
+
+write_csv(weeks, "weeks_chart.csv")
+
+read_csv("data/first_score_by_week.csv") |>
+  filter(G_Score1st == 1,
+         week >= 9) |> # look just at teams that scored first in a game
+  summarize(win_percentage = sum(W) / n())
